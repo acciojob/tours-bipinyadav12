c848c4cc6d6fdd0f1c02e8a1dba41cd382f19581
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2025-01-14 17:08:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import 'regenerator-runtime/runtime';
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
