47ae6c64cd35aaf2aa0eb620e1e86b12a1f7367c
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2025-01-14 16:38:11 +0530

    Update Tour.js

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,27 +1,29 @@
 import React, { useState } from 'react';
 
-const Tour = ({ tour, removeTour }) => {
-  const { id, name, info, image, price } = tour;
-  const [readMore, setReadMore] = useState(false);
+function Tour({ id, name, info, image, price, removeTour }) {
+  const [showMore, setShowMore] = useState(false);
 
   return (
     <div className="tour">
       <img src={image} alt={name} className="tour-image" />
       <div className="tour-details">
-        <h4>{name}</h4>
-        <p className="tour-price">${price}</p>
+        <h3 className="tour-name">{name}</h3>
+        <h4 className="tour-price">${price}</h4>
         <p className="tour-info">
-          {readMore ? info : `${info.substring(0, 200)}...`}
-          <button onClick={() => setReadMore(!readMore)}>
-            {readMore ? 'See less' : 'Read more'}
+          {showMore ? info : `${info.substring(0, 200)}...`}
+          <button
+            onClick={() => setShowMore(!showMore)}
+            className="btn-show-more"
+          >
+            {showMore ? 'Show less' : 'Read more'}
           </button>
         </p>
-        <button className="delete-btn" onClick={() => removeTour(id)}>
+        <button onClick={() => removeTour(id)} className="btn-remove">
           Not Interested
         </button>
       </div>
     </div>
   );
-};
+}
 
 export default Tour;
